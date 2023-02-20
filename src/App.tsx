7f5f4cf96aab5ffb8994e537b12f130624dce66c
Sun <ssd3237649@gmail.com>
2023-02-20 06:53:28 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import './App.css'
+
+const MyDiv = styled.div `
+  height: 200px;
+  background: blue;
+`
+export default function App() {
+  return (
+    <main>
+      <MyDiv />
+      React ⚛️ + Vite ⚡ + Replit 🌀 xxxx
+    </main>
+  )
+}
