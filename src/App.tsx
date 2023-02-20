050f5463fba0c6785cc5187f45a0db249a757dc6
sunshengda <sunshengda@bytedance.com>
2023-02-20 18:20:36 +0800

    test: make a confict

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <main>
       <MyDiv />
-      React ⚛️ + Vite ⚡ + Replit 🌀 xxxx1111
+      React ⚛️ + Vite ⚡ + Replit 🌀 xxxx11112222
     </main>
   )
 }
